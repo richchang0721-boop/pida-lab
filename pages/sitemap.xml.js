1d6fd97ebcc37d4afafb42acca133b7eff2aad6a
richchang0721-boop <richchang0721@gmail.com>
2026-05-06 13:43:25 +0800

    update sitemap.xml

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,25 +1,32 @@
+import { getSortedPostsData } from '../lib/posts.server'
+
 export default function Sitemap() {}
 
 export async function getServerSideProps({ res }) {
-  const baseUrl = 'https://pida-lab.com'
+  const baseUrl = 'https://www.pida-lab.com'
 
-  const pages = [
+  // Static pages
+  const staticPages = [
     '',
-    '/posts/pida-entry-point',
-    '/posts/why-ai-alignment-might-be-solving-the-wrong-problem',
+    '/demo',
+    '/about',
+    '/contact',
+    '/posts',
+    '/categories',
   ]
 
+  // Dynamic: auto-generate from all posts
+  const allPosts = getSortedPostsData()
+  const postPages = allPosts.map(post => `/posts/${post.id}`)
+
+  const allPages = [...staticPages, ...postPages]
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map(
-        (page) => `
-      <url>
-        <loc>${baseUrl}${page}</loc>
-      </url>`
-      )
-      .join('')}
-  </urlset>`
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allPages.map(page => `  <url>
+    <loc>${baseUrl}${page}</loc>
+  </url>`).join('\n')}
+</urlset>`
 
   res.setHeader('Content-Type', 'text/xml')
   res.write(sitemap)
